fbe941bdded03d8750a1b7b3344e041e05544437
Andrew Fore <andrewrfore@gmail.com>
2025-11-09 16:08:06 -0500

    Updated

---
+++ Documents/hyprland-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo pacman --needed -S hyprland-git hyprcursor-git hyprgraphics-git hypridle hyprland-protocols-git hyprland-guiutils-git hyprland-qt-support hyprlang-git hyprtoolkit-git hyprlock hyprpaper hyprpicker hyprutils-git hyprwayland-scanner-git hyprpolkitagent aquamarine-git bat brightnessctl cmake distrobox docker fd file-roller flatpak flatpak-xdg-utils git gnome-keyring grim gvfs helix kvantum kvantum-qt5 kitty libreoffice-fresh lxappearance meson neovim gtk-layer-shell ninja nwg-dock-hyprland nwg-bar nwg-clipman nwg-drawer nwg-look papirus-icon-theme pavucontrol pipewire playerctl podman protobuf qt5-wayland qt5ct qt6-wayland qt6ct ripgrep seahorse slurp starship swaync thunar thunar-archive-plugin udiskie waybar wf-recorder wireplumber wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-hyprland xdg-user-dirs zsh nwg-dock-hyprland nwg-drawer gtk-layer-shell nwg-bar nwg-clipman libreoffice-fresh distrobox docker file-roller flatpak flatpak-xdg-utils gnome-keyring gtk-layer-shell kvantum kvantum-qt5 libreoffice-fresh linux-firmware-qlogic nwg-bar nwg-clipman nwg-dock-hyprland nwg-drawer nwg-look papirus-icon-theme podman qt5-wayland qt5ct qt6-wayland qt6ct seahorse swaync thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin thunar-vcs-plugin thunar-volman udiskie waybar wf-recorder wl-clipboard xdg-desktop-portal-gtk xdg-desktop-portal-hyprland wl-clip-persist uwsm sddm ttf-hack ttf-hack-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-cascadia-code ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-ibm-plex ttf-ibmplex-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono otf-font-awesome 7zip uwsm rustup network-manager-applet blueman blueberry kdeconnect alacritty cpio bitwarden bitwarden-cli unzip parted
+
+#yay --needed -S hyprland-welcome-git ulauncher-git apple-fonts 1password-beta 1password-cli tui-1password-git brave-bin aic94xx-firmware ast-firmware wd719x-firmware upd72020x-fw spotify hyprpaper waypaper swayosd asdf-vm spicetify
+
+
+
+
+
+
